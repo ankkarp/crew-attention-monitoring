02d1cef52703994f9652112b201573d98d21e879
OlegSivets <legolas2002@mail.ru>
2023-09-24 01:58:07 +0300

    interface was updated

--- interface.py
+++ interface.py
@@ -3,6 +3,7 @@ import gradio as gr
 from model import *
 from analysis import *
 import argparse
+# import moviepy.editor as moviepy
 
 parser = argparse.ArgumentParser()
 
@@ -14,41 +15,39 @@ args = parser.parse_args()
 
 
 inputs = [
-    gr.Video(format='mp4'),
+    gr.Video(type='filepath', label='Input Video'),
+
+
     # gr.Textbox(lines=2, placeholder="0", label='Введите номер нарушения')
 ]
 
 outputs = [
-    "playable_video",
     gr.Dataframe(
         label="Результат обработки видео",
         # value=df,
         max_rows=10,
         col=2,
     ),
-    # gr.Gallery(label="Фиксация нарушения", columns=(1, 3)),
+    gr.Gallery(label="Фиксация нарушения", columns=(1, 3)),
     # gr.CheckboxGroup([str(i) for i in range(df.shape[0])], label="Проверка", info="Было ли нарушение?")
 ]
 
 
-def logic(video):
-    type(video)
-    # df, img = some_model(video)
+def logic(video_path, input_value):
     pd_ans = None
+    image = None
 
-    if video:
-
+    if video_path:
         model = AttentionModel()
         model.load_models(args.detection_model, args.pose_model)
-        model.process_video('data_path', 'out_path')
+        model.process_video(video_path, 'temp/output_video.avi')
 
         analyzer = Analyzer()
         violations = analyzer.process_model_data(model.pos_est_model, model.detected_data)
         ans = analyzer.process_violations(violations)
 
         pd_ans = pd.DataFrame(ans, columns=['Start_time', 'End_time'])
-
-    return video, pd_ans
+    return pd_ans, image
 
 
 demo = gr.Interface(
