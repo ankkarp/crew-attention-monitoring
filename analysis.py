ad9ca4fdd8b163e72e1acb510577f389be96b4e5
ankkarp <anna.a.k.2002@gmail.com>
2023-09-23 23:44:30 +0300

    analyzer update

--- analysis.py
+++ analysis.py
@@ -23,16 +23,18 @@ class Analyzer:
                 phones = detection_data.loc[frame_id, 'Position'].copy()
 
                 for i in range(len(persons_wrists)):
-                    for wrists in persons_wrists[i]:
-                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - wrists), phones))
+
+                    for j in range(len(persons_wrists[i])):
+                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - persons_wrists[i][j]), phones))
                         min_dist = min(dists)
 
-                        total_p_sec = time / 1000
-                        p_min = int(total_p_sec // 60)
-                        p_sec = int(total_p_sec % 60)
+                        closest_id = dists.index(min_dist)
+                        closest_phone = phones[closest_id]
 
                         if min_dist <= self.max_wrist_dist:
-                            violations.append([frame_id, time, min_dist, f'min:{p_min}:{p_sec}'])
+                            if not ((j == 0 and persons_wrists[i][j][0] > closest_phone[2])
+                                    or (j == 1 and persons_wrists[i][j][0] < closest_phone[0])):
+                                violations.append([frame_id, time])
         return violations
 
     def _update_violations(self, violations_processed, passed_time, violation, start_time):
@@ -53,7 +55,7 @@ class Analyzer:
         passed_time = 0
         start_time = -1
 
-        for i in tqdm(range(0, len(violations) - 1)):
+        for i in range(0, len(violations) - 1):
             time_diff = violations[i + 1][1] - violations[i][1]
 
             if (passed_time < 3000 and time_diff <= 2000) or (passed_time > 3000 and time_diff <= 10000):
@@ -62,20 +64,28 @@ class Analyzer:
                 passed_time += time_diff
 
                 if i == len(violations) - 2:
-                    violations_processed = self._update_violations(violations_processed, passed_time,
-                                                                   violations[i], start_time)
 
+                    if passed_time > 3000:
+                        total_sec = start_time / 1000
+                        min = int(total_sec // 60)
+                        sec = int(total_sec % 60)
+
+                        total_p_sec = (start_time + passed_time) / 1000
+                        p_min = int(total_p_sec // 60)
+                        p_sec = int(total_p_sec % 60)
+
+                        violations_processed.append(f'{min}:{sec}, end: {p_min}:{p_sec}')
             else:
-                if passed_time > 3000 and violations[i][2]:  # min_wrist_dist
+                if passed_time > 3000:
                     total_sec = start_time / 1000
                     min = int(total_sec // 60)
                     sec = int(total_sec % 60)
 
-                    total_p_sec = passed_time / 1000
+                    total_p_sec = (start_time + passed_time) / 1000
                     p_min = int(total_p_sec // 60)
                     p_sec = int(total_p_sec % 60)
 
-                    violations_processed.append(f'{min}:{sec}, passed_time: {p_min}:{p_sec}')
+                    violations_processed.append(f'start: {min}:{sec}, end: {p_min}:{p_sec}')
 
                 passed_time = 0
                 start_time = -1
